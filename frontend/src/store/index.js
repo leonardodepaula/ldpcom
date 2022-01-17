87336fe35d23ede2ae9dca011bef0788b8337e8f
Leonardo de Paula Liebscher <leonardo.de.paula@rfb.gov.br>
2022-01-17 10:26:14 -0300

    Vuex Persisted State

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 
 import SideBarModule from "./modules/sidebar.js"
 
 const store = new createStore({
   modules: {
     sidebarmodule: SideBarModule
-  }
+  },
+  plugins: [createPersistedState()]
 })
 
 export default store;
