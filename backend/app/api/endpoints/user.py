90a04001b0469afcb88c345ba85344a61b3969c5
Leonardo de Paula Liebscher <leonardo.de.paula@rfb.gov.br>
2022-01-18 00:46:08 -0300

    List and delete users

--- backend/app/api/endpoints/user.py
+++ backend/app/api/endpoints/user.py
@@ -9,6 +9,14 @@ from app.core import dependencies
 
 router = APIRouter()
 
+@router.get("/", response_model=List[schemas.User])
+def read_users(db: Session = Depends(dependencies.get_db), skip: int = 0, limit: int = 100) -> Any:
+    """
+    Retrieve users.
+    """
+    users = crud.user.get_multi(db, skip=skip, limit=limit)
+    return users
+
 @router.post("/", response_model=schemas.User)
 def create_user(*, db: Session = Depends(dependencies.get_db), user_in: schemas.UserCreate) -> Any:
     """
@@ -32,4 +40,16 @@ def read_user_by_id(*, user_id: int, db: Session = Depends(dependencies.get_db))
     if not user:
         raise HTTPException(status_code=404, detail="User not found.")
     else:
+        return user
+
+@router.delete('/{user_id}', response_model=schemas.User)
+def delete_user(*, db: Session = Depends(dependencies.get_db), user_id: int) -> Any:
+    """
+    Delete a specific user by id.
+    """
+    user = crud.user.get(db=db, id=user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    else:
+        user = crud.user.remove(db=db, id=user_id)
         return user
