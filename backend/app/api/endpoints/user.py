b0881f07d3c7781a0f287aca0ce7124cd06dc66f
Leonardo de Paula Liebscher <leonardo.de.paula@rfb.gov.br>
2022-01-18 09:24:17 -0300

    FastAPI auth improvements

--- backend/app/api/endpoints/user.py
+++ backend/app/api/endpoints/user.py
@@ -4,52 +4,63 @@ from fastapi import APIRouter, Depends
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
 
-from app import crud, schemas
+from app import crud, models, schemas
 from app.core import dependencies
 
 router = APIRouter()
 
-@router.get("/", response_model=List[schemas.User])
-def read_users(db: Session = Depends(dependencies.get_db), skip: int = 0, limit: int = 100) -> Any:
-    """
+@router.get('/', response_model=List[schemas.User])
+def read_users(db: Session = Depends(dependencies.get_db), skip: int = 0, limit: int = 100, current_user: models.User = Depends(dependencies.get_current_active_superuser)) -> Any:
+    '''
     Retrieve a list of users.
-    """
+    '''
     users = crud.user.get_multi(db, skip=skip, limit=limit)
     return users
 
-@router.post("/", response_model=schemas.User)
-def create_user(*, db: Session = Depends(dependencies.get_db), user_in: schemas.UserCreate) -> Any:
-    """
+@router.post('/', response_model=schemas.User)
+def create_user(*, db: Session = Depends(dependencies.get_db), user_in: schemas.UserCreate, current_user: models.User = Depends(dependencies.get_current_active_superuser)) -> Any:
+    '''
     Create new user.
-    """
+    '''
     user = crud.user.get_by_email(db, email=user_in.email)
     if user:
         raise HTTPException(
             status_code=400,
-            detail="The user with this username already exists in the system.",
+            detail='The user with this username already exists in the system.',
         )
     user = crud.user.create(db, obj_in=user_in)
     return user
 
-@router.get("/{id}", response_model=schemas.User)
-def read_user_by_id(*, id: int, db: Session = Depends(dependencies.get_db)) -> Any:
-    """
+@router.get('/{id}', response_model=schemas.User)
+def read_user_by_id(*, id: int, db: Session = Depends(dependencies.get_db), current_user: models.User = Depends(dependencies.get_current_active_user)) -> Any:
+    '''
     Get a specific user by id.
-    """
+    '''
     user = crud.user.get(db, id=id)
     if not user:
-        raise HTTPException(status_code=404, detail="User not found.")
+        raise HTTPException(status_code=404, detail='User not found.')
     else:
         return user
 
 @router.delete('/{id}', response_model=schemas.User)
-def delete_user(*, db: Session = Depends(dependencies.get_db), id: int) -> Any:
-    """
+def delete_user(*, db: Session = Depends(dependencies.get_db), id: int, current_user: models.User = Depends(dependencies.get_current_active_superuser)) -> Any:
+    '''
     Delete a specific user by id.
-    """
+    '''
     user = crud.user.get(db=db, id=id)
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail='User not found')
     else:
         user = crud.user.remove(db=db, id=id)
         return user
+
+@router.put('/{id}', response_model=schemas.User)
+def update_user(*, db: Session = Depends(dependencies.get_db), id: int, user_in: schemas.UserUpdate, current_user: models.User = Depends(dependencies.get_current_active_superuser)) -> Any:
+    '''
+    Update a user.
+    '''
+    user = crud.user.get(db, id=id)
+    if not user:
+        raise HTTPException(status_code=404, detail='The user with this username does not exist in the system')
+    user = crud.user.update(db, db_obj=user, obj_in=user_in)
+    return user
--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -32,18 +32,18 @@ def get_current_user(db: Session = Depends(get_db), token: str = Depends(reusabl
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ENCRYPTION_ALGORITHM])
         token_data = schemas.TokenPayload(**payload)
     except (jwt.JWTError, ValidationError):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Could not validate credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Could not validate credentials')
     user = crud.user.get(db, id=token_data.sub)
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail='User not found')
     return user
 
 def get_current_active_user(current_user: models.User = Depends(get_current_user)) -> models.User:
     if not crud.user.is_active(current_user):
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(status_code=400, detail='Inactive user')
     return current_user
 
 def get_current_active_superuser(current_user: models.User = Depends(get_current_user)) -> models.User:
     if not crud.user.is_superuser(current_user):
         raise HTTPException(status_code=400, detail="The user doesn't have enough privileges")
-    return current_user
+    return current_use
