d0b623f9b296b00e601e58f11e8faa27f407f166
Leonardo de Paula Liebscher <leonardo.de.paula@rfb.gov.br>
2022-01-18 08:29:48 -0300

    Minor adjustments

--- backend/app/api/endpoints/user.py
+++ backend/app/api/endpoints/user.py
@@ -9,10 +9,11 @@ from app.core import dependencies
 
 router = APIRouter()
 
+
 @router.get("/", response_model=List[schemas.User])
 def read_users(db: Session = Depends(dependencies.get_db), skip: int = 0, limit: int = 100) -> Any:
     """
-    Retrieve users.
+    Retrieve a list of users.
     """
     users = crud.user.get_multi(db, skip=skip, limit=limit)
     return users
@@ -31,25 +32,25 @@ def create_user(*, db: Session = Depends(dependencies.get_db), user_in: schemas.
     user = crud.user.create(db, obj_in=user_in)
     return user
 
-@router.get("/{user_id}", response_model=schemas.User)
-def read_user_by_id(*, user_id: int, db: Session = Depends(dependencies.get_db)) -> Any:
+@router.get("/{id}", response_model=schemas.User)
+def read_user_by_id(*, id: int, db: Session = Depends(dependencies.get_db)) -> Any:
     """
     Get a specific user by id.
     """
-    user = crud.user.get(db, id=user_id)
+    user = crud.user.get(db, id=id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found.")
     else:
         return user
 
-@router.delete('/{user_id}', response_model=schemas.User)
-def delete_user(*, db: Session = Depends(dependencies.get_db), user_id: int) -> Any:
+@router.delete('/{id}', response_model=schemas.User)
+def delete_user(*, db: Session = Depends(dependencies.get_db), id: int) -> Any:
     """
     Delete a specific user by id.
     """
-    user = crud.user.get(db=db, id=user_id)
+    user = crud.user.get(db=db, id=id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     else:
-        user = crud.user.remove(db=db, id=user_id)
+        user = crud.user.remove(db=db, id=id)
         return user
