a3ad82b6fa62c840f03317de4f3cee5f990c1e5b
Leonardo de Paula Liebscher <leonardo.de.paula@rfb.gov.br>
2022-02-16 12:48:05 -0300

    FastAPI StaticFiles

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 from app.core.config import settings
 from app.api.base import api_router
@@ -18,3 +19,5 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+app.mount('/static', StaticFiles(directory='app/static'), name='static')
